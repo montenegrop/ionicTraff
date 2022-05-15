2f6205b3ae79ebe13fa0b4d93b2ebd18c5075e6b
montenegrop <pablo.rpm@gmail.com>
2022-05-15 03:08:39 -0300

    newPage grid

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import '@ionic/react/css/display.css'
 
 /* Theme variables */
 import './theme/variables.css'
+import GridPage from './pages/gridPage'
 
 setupIonicReact()
 
@@ -33,6 +34,9 @@ const App: React.FC = () => (
                 </Route>
                 <Route exact path="/">
                     <Redirect to="/home" />
+                </Route>
+                <Route exact path="/grid">
+                    <GridPage></GridPage>
                 </Route>
             </IonRouterOutlet>
         </IonReactRouter>
---
+++ src/pages/gridPage.tsx
@@ -0,0 +1,6 @@
+import { GridExample } from '../components/grid'
+
+const GridPage: React.FC = () => {
+    return <GridExample></GridExample>
+}
+export default GridPage
